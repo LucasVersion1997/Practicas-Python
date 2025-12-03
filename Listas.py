5be31ac752b70ee717e0df661d511f26482978b5
LucasVersion1997 <lucaspedernerax2@gmail.com>
2025-12-02 22:19:39 -0300

    Repasando comandos

---
+++ Listas.py
@@ -0,0 +1,11 @@
+#COMANDOS PARA LAS LISTAS!! 
+list () #crear
+len (lista) #Cantidad de elementos de la tabla
+lista.append #Agrego algo a la lista
+lista.insert(2, "Taylor") #Agregamos algo en cierto indice 
+lista.extended #Agregamos varios elementos a la lista
+lista.pop (3) # Eliminamos elementos por su indice
+lista.remove ("Nombre") #Sacamos un elemento por su nombre 
+lista.clear #Borramos todos los elementos
+lista.copy #Para copiar la lista
+lista.sort #Para ordenar la lista
---
+++ Variables.py
@@ -0,0 +1,18 @@
+#PRIMER PRINT
+#Podemos apretar en cierto punto y parar el codigo ahi para "depurar"
+
+#Que es una variable
+
+my_string_variable=("Lucas")
+print (my_string_variable)
+
+my_int_variable =(28)
+print (my_int_variable)
+
+print (my_string_variable, my_int_variable)
+print ("Este es el valor de:" , my_int_variable)
+
+nombre =input("What is your name:")
+edad = input("How old are you?: ")
+
+print ("Tu nombre es", nombre, "Tu edad es", edad)
