69661228e2ee4626c3b5d3fe0ab0dd6a51c8bd44
LucasVersion1997 <lucaspedernerax2@gmail.com>
2025-12-10 13:26:20 -0300

    Agregue tuplas y el diccionario

--- Listas.py
+++ Listas.py
@@ -1,11 +1,13 @@
 #COMANDOS PARA LAS LISTAS!! 
-list () #crear
-len (lista) #Cantidad de elementos de la tabla
+list [] #crear
+len #Cantidad de elementos de la tabla
 lista.append #Agrego algo a la lista
-lista.insert(2, "Taylor") #Agregamos algo en cierto indice 
+lista.insert[2, "Taylor"] #Agregamos algo en cierto indice 
 lista.extended #Agregamos varios elementos a la lista
-lista.pop (3) # Eliminamos elementos por su indice
-lista.remove ("Nombre") #Sacamos un elemento por su nombre 
+lista.pop [3] # Eliminamos elementos por su indice
+lista.remove ["Nombre"] #Sacamos un elemento por su nombre 
 lista.clear #Borramos todos los elementos
 lista.copy #Para copiar la lista
 lista.sort #Para ordenar la lista
+ 
+# Mutables (Se pueden cambiar). Se pueden añadir, eliminar, modificar o reordenar elementos después de su creación.
